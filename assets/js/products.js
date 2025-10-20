62eb6bd3dff616754feac522d2451b8d08ef06ce
Hleizo <zeinddin.kamel@gmail.com>
2025-10-20 21:25:56 +0300

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -10,9 +10,7 @@ window.PRODUCTS = [
     colors: ["أسود"],
     sizes: ["S","M","L","XL"],
     images: [
-      "https://images.unsplash.com/photo-1520975916090-3105956dac38?q=80&w=1200&auto=format&fit=crop",
-      "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?q=80&w=1200&auto=format&fit=crop",
-      "https://images.unsplash.com/photo-1602810318383-9e3d9d5b1d5f?q=80&w=1200&auto=format&fit=crop"
+assets/products/abaya-with-flowers.png
     ],
     description: "قماش خفيف بملمس حريري وانسيابية مريحة؛ خيار يومي أنيق."
   },
